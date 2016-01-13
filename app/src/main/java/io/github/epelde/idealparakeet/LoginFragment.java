882356986df28073780e5c376e8fedaa9b1ad6d9
Alex Epelde <aepelde@gmail.com>
2016-01-13 11:23:09 +0100

    Removing comments

--- app/src/main/java/io/github/epelde/idealparakeet/LoginFragment.java
+++ app/src/main/java/io/github/epelde/idealparakeet/LoginFragment.java
@@ -8,7 +8,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,7 +54,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        Log.i(LOG_TAG, "* * * LoginFragment RESUMED");
         Uri uri = getActivity().getIntent().getData();
         if (uri != null && uri.toString().startsWith(App.REDIRECT_URI)) {
             String code = uri.getQueryParameter("code");
